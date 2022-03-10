a014736269cf0d685950b6c6d9a16b9f8ff44d7e
Martino888 <maxmartino33@gmail.com>
2022-03-10 14:52:56 +0100

    VUE

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = new Vue({
+    el:'#container',
+    data:{
+
+        titolo:'HelloWorld',
+        srcImage:'https://picsum.photos/200/300',
+        altImage:'image',
+    }
+});
